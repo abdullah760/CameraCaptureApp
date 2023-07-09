0d5598158d1bfdb3cc12bdd82a7ad35112a9aad1
Abdullah760 <abdoassem11@gmail.com>
2023-07-08 21:05:06 -0700

    Initial Commit

---
+++ CameraView/CameraViewApp.swift
@@ -0,0 +1,17 @@
+//
+//  CameraViewApp.swift
+//  CameraView
+//
+//  Created by Abdullah on 08/07/2023.
+//
+
+import SwiftUI
+
+@main
+struct CameraViewApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
